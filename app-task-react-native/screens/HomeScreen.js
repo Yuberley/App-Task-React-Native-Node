f9709c15b48b836ee21d612cf68999a1d628ba0d
Yuberley <yuberleyguerrero1999@gmail.com>
2023-01-01 16:49:35 -0500

    first commit

---
+++ app-task-react-native/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreen = () => {
+
+    useEffect(() => {
+        console.log('HomeScreen mounted')
+        return () => {
+            console.log('HomeScreen unmounted')
+        }
+    }, [])
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
